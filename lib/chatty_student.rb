a5d16fb5c0d860560087a92d5e641eda46a55bdc
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-07-05 19:42:26 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,8 +6,14 @@ class ChattyStudent < Student
 
   def raise_hand
     super
-    10.times do
-      puts "Pick me!"
-    end
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
   end
 end
